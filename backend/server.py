4c3fc25d4cafa89c58c7cbc89fd99f2f255ddb70
sirrah23 <hseth93@gmail.com>
2017-07-12 23:28:09 -0400

    Serving front-end webpage from Flask now

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -30,6 +30,11 @@ class Planner(Resource):
                 }
 
 api.add_resource(Planner, '/planner')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
